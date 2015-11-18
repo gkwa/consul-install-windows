5b85edee742a55ba513c73654e9fe856f09483e6
Taylor Monacelli <taylor.monacelli@streambox.com>
2015-11-18 10:02:03 -0800

    Calling external program from PS doesnt require ampersand

--- consulinstall.ps1
+++ consulinstall.ps1
@@ -1,19 +1,19 @@
 function open_firewall()
 {
     # firewall
-    & netsh advfirewall firewall delete rule name="Consul HashiCorp UDP" protocol=UDP localport="8300,8301,8302,8400,8500,8600" | out-null
-    & netsh advfirewall firewall delete rule name="Consul HashiCorp TCP" protocol=TCP localport="8300,8301,8302,8400,8500,8600" | out-null
+    netsh advfirewall firewall delete rule name="Consul HashiCorp UDP" protocol=UDP localport="8300,8301,8302,8400,8500,8600" | out-null
+    netsh advfirewall firewall delete rule name="Consul HashiCorp TCP" protocol=TCP localport="8300,8301,8302,8400,8500,8600" | out-null
 
     # allow binary
     ##############################
-    & netsh advfirewall firewall delete rule name="Consul HashiCorp App" program="C:\ProgramData\consul\consul.exe" | out-null
-    & netsh advfirewall firewall add rule name="Consul HashiCorp App" dir=in action=allow program="C:\ProgramData\consul\consul.exe" enable=yes | out-null
+    netsh advfirewall firewall delete rule name="Consul HashiCorp App" program="C:\ProgramData\consul\consul.exe" | out-null
+    netsh advfirewall firewall add rule name="Consul HashiCorp App" dir=in action=allow program="C:\ProgramData\consul\consul.exe" enable=yes | out-null
 
     ##############################
 
     # tcp/8300 for RPC
-    & netsh advfirewall firewall delete rule name="Consul HashiCorp RPC" dir=in protocol=TCP localport=8300 | out-null
-    & netsh advfirewall firewall add rule name="Consul HashiCorp RPC" dir=in action=allow protocol=TCP localport=8300 | out-file install.log
+    netsh advfirewall firewall delete rule name="Consul HashiCorp RPC" dir=in protocol=TCP localport=8300 | out-null
+    netsh advfirewall firewall add rule name="Consul HashiCorp RPC" dir=in action=allow protocol=TCP localport=8300 | out-file install.log
 
     ##############################
 
@@ -21,12 +21,12 @@ function open_firewall()
     # LAN. Required by all agents. TCP and UDP.
 
     # TCP/8301 for Serf LAN
-    & netsh advfirewall firewall delete rule name="Consul HashiCorp Serf LAN (TCP)" dir=in protocol=TCP localport=8301 | out-null
-    & netsh advfirewall firewall add rule name="Consul HashiCorp Serf LAN (TCP)" dir=in action=allow protocol=TCP localport=8301 | out-file install.log
+    netsh advfirewall firewall delete rule name="Consul HashiCorp Serf LAN (TCP)" dir=in protocol=TCP localport=8301 | out-null
+    netsh advfirewall firewall add rule name="Consul HashiCorp Serf LAN (TCP)" dir=in action=allow protocol=TCP localport=8301 | out-file install.log
 
     # UDP/8301 for Serf LAN
-    & netsh advfirewall firewall delete rule name="Consul HashiCorp Serf LAN (UDP)" dir=in protocol=UDP localport=8301 | out-null
-    & netsh advfirewall firewall add rule name="Consul HashiCorp Serf LAN (UDP)" dir=in action=allow protocol=UDP localport=8301 | out-file install.log
+    netsh advfirewall firewall delete rule name="Consul HashiCorp Serf LAN (UDP)" dir=in protocol=UDP localport=8301 | out-null
+    netsh advfirewall firewall add rule name="Consul HashiCorp Serf LAN (UDP)" dir=in action=allow protocol=UDP localport=8301 | out-file install.log
 
     ##############################
 
@@ -34,12 +34,12 @@ function open_firewall()
     # WAN. Required by all agents. TCP and UDP.
 
     # TCP/8400 for Serf WAN
-    & netsh advfirewall firewall delete rule name="Consul HashiCorp Serf WAN (TCP)" dir=in protocol=TCP localport=8400 | out-null
-    & netsh advfirewall firewall add rule name="Consul HashiCorp Serf WAN (TCP)" dir=in action=allow protocol=TCP localport=8400 | out-file install.log
+    netsh advfirewall firewall delete rule name="Consul HashiCorp Serf WAN (TCP)" dir=in protocol=TCP localport=8400 | out-null
+    netsh advfirewall firewall add rule name="Consul HashiCorp Serf WAN (TCP)" dir=in action=allow protocol=TCP localport=8400 | out-file install.log
 
     # UDP/8400 for Serf WAN
-    & netsh advfirewall firewall delete rule name="Consul HashiCorp Serf WAN (UDP)" dir=in protocol=UDP localport=8400 | out-null
-    & netsh advfirewall firewall add rule name="Consul HashiCorp Serf WAN (UDP)" dir=in action=allow protocol=UDP localport=8400 | out-file install.log
+    netsh advfirewall firewall delete rule name="Consul HashiCorp Serf WAN (UDP)" dir=in protocol=UDP localport=8400 | out-null
+    netsh advfirewall firewall add rule name="Consul HashiCorp Serf WAN (UDP)" dir=in action=allow protocol=UDP localport=8400 | out-file install.log
 
     ##############################
 
@@ -47,8 +47,8 @@ function open_firewall()
     # API. TCP only.
 
     # TCP/8500 for HTTP API
-    & netsh advfirewall firewall delete rule name="Consul HashiCorp HTTP API" dir=in protocol=TCP localport=8500 | out-null
-    & netsh advfirewall firewall add rule name="Consul HashiCorp HTTP API" dir=in action=allow protocol=TCP localport=8500 | out-file install.log
+    netsh advfirewall firewall delete rule name="Consul HashiCorp HTTP API" dir=in protocol=TCP localport=8500 | out-null
+    netsh advfirewall firewall add rule name="Consul HashiCorp HTTP API" dir=in action=allow protocol=TCP localport=8500 | out-file install.log
 
     ##############################
 
@@ -56,8 +56,8 @@ function open_firewall()
     # UDP.
 
     # TCP/8600 DNS
-    & netsh advfirewall firewall delete rule name="Consul HashiCorp DNS queries" dir=in protocol=TCP localport=8600 | out-null
-    & netsh advfirewall firewall add rule name="Consul HashiCorp DNS queries" dir=in action=allow protocol=TCP localport=8600 | out-file install.log
+    netsh advfirewall firewall delete rule name="Consul HashiCorp DNS queries" dir=in protocol=TCP localport=8600 | out-null
+    netsh advfirewall firewall add rule name="Consul HashiCorp DNS queries" dir=in action=allow protocol=TCP localport=8600 | out-file install.log
 }
 
 $env:path = "$pwd;$env:path"
@@ -102,9 +102,9 @@ $env:path = "$pwd;$env:path"
 # Overwrite config
 (new-object System.Net.WebClient).DownloadFile($consul_config_url, "config.hcl")
 
-& 7za x -y $consul_zip | out-null
+7za x -y $consul_zip | out-null
 
-& 7za x -y -ocweb $consul_www_zip | out-null
+7za x -y -ocweb $consul_www_zip | out-null
 
 
 
@@ -116,7 +116,7 @@ foreach ($service in $services)
     {
         $service | Stop-Service
     }
-    & nssm remove Consul confirm | out-file install.log
+    nssm remove Consul confirm | out-file install.log
 }
 
 $consul = Get-Process Consul -ErrorAction SilentlyContinue
@@ -158,12 +158,12 @@ set-location C:\ProgramData\consul
 # C:\ProgramData\consul
 $env:path = "C:\ProgramData\consul;$env:path"
 
-& nssm install Consul C:\ProgramData\consul\consul.exe confirm | out-file install.log
-& nssm set Consul AppDirectory C:\ProgramData\consul | out-file install.log
-& nssm set Consul AppParameters agent -server -config-file C:\ProgramData\consul\config\config.hcl | out-file install.log
-& nssm set Consul DisplayName Consul | out-file install.log
-& nssm set Consul Description Consul from HashiCorp | out-file install.log
-& nssm set Consul Start SERVICE_AUTO_START | out-file install.log
+nssm install Consul C:\ProgramData\consul\consul.exe confirm | out-file install.log
+nssm set Consul AppDirectory C:\ProgramData\consul | out-file install.log
+nssm set Consul AppParameters agent -server -config-file C:\ProgramData\consul\config\config.hcl | out-file install.log
+nssm set Consul DisplayName Consul | out-file install.log
+nssm set Consul Description Consul from HashiCorp | out-file install.log
+nssm set Consul Start SERVICE_AUTO_START | out-file install.log
 open_firewall
 
 
@@ -172,6 +172,6 @@ if(test-path "$env:windir\system32\consul.exe")
     remove-item $env:windir\system32\consul.exe -Force
 }
 
-& cmd /c mklink $env:windir\system32\consul.exe C:\ProgramData\consul\consul.exe | out-file install.org
+mklink $env:windir\system32\consul.exe C:\ProgramData\consul\consul.exe | out-file install.org
 
-& nssm start Consul | out-file install.log
+nssm start Consul | out-file install.log
